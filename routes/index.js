832c316b8c8b1783690e589f83cd265fc4184585
Nitin gour <gournitin39@gmail.com>
2024-06-20 01:09:09 +0530

    create ejs pages and routes : contact about log in forget page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'home page' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about page' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'log in ' });
 });
 
 module.exports = router;
